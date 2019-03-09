dc9fdf87b35f4fb67ace67fe9ed6d642bd0fa5fb
PythonPickle <48324094+PythonPickle@users.noreply.github.com>
2019-03-09 16:59:39 -0500

    Delete settingsV2.py

--- settingsV2.py
+++
@@ -1,8 +0,0 @@
-class Settings:
-    """Class to store settings for Ball Brick Break 2"""
-    def __init__(self):
-        """Initialize game settings"""
-        # Screen Settings
-        self.screen_width = 800
-        self.screen_height = 600
-        self.bg_color = (255, 130, 130)
